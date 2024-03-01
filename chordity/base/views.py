157c3dff935aed02d9f772d4e933c4f83c2c4e7f
Rex Herndon <herndonrex1@gmail.com>
2024-02-29 18:51:48 -0600

    worked on search funct and started user login

--- chordity/base/views.py
+++ chordity/base/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Room
+from django.db.models import Q
+from django.contrib.auth.models import User
+from .models import Room, Topic
 from .forms import RoomForm
 
 # Create your views here.
@@ -11,12 +13,37 @@ from .forms import RoomForm
 #     {'id':3, 'name':'Most common keys in pop?'},
 # ]
 
+
+def loginPage(request):
+
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        
+        try:
+            user = User.objects.get(username=username)
+        except:
+        
+    context = {}
+    return render(request, "base/login_register.html", context)
+
+
 def home(request):
+    q = request.GET.get("q") if request.GET.get("q") != None else ""
+
     # returns all instances of room objects
-    rooms = Room.objects.all()
-    
-    context = {'rooms': rooms}
-    return render(request, 'base/home.html', context)
+    # icontains filters by characters that are found in the search and is not case sensitive
+    # for example, typing "yo" in the search bar searches for YouTube on google
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) | Q(name__icontains=q) | Q(description__icontains=q)
+    )
+
+    topics = Topic.objects.all()
+    room_count = rooms.count()
+
+    context = {"rooms": rooms, "topics": topics, "room_count": room_count}
+    return render(request, "base/home.html", context)
+
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
@@ -24,39 +51,41 @@ def room(request, pk):
     #     if i['id'] == int(pk):
     #         room = i
 
-    context = {'room': room} 
+    context = {"room": room}
 
-    return render(request, 'base/room.html', context)
+    return render(request, "base/room.html", context)
+
 
 def createRoom(request):
     form = RoomForm()
-    
-    if request.method == 'POST':
+
+    if request.method == "POST":
         form = RoomForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('home')
-    
-    context = {'form': form}
-    return render(request, 'base/room_form.html', context)
+            return redirect("home")
+
+    context = {"form": form}
+    return render(request, "base/room_form.html", context)
 
 
 def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
-    
-    if request.method == 'POST':
+
+    if request.method == "POST":
         form = RoomForm(request.POST, instance=room)
         if form.is_valid():
             form.save()
-            return redirect('home')
-    
-    context = {'form': form}
-    return render(request, 'base/room_form.html', context)
+            return redirect("home")
+
+    context = {"form": form}
+    return render(request, "base/room_form.html", context)
+
 
 def deleteRoom(request, pk):
     room = Room.objects.get(id=pk)
-    if request.method == 'POST':
+    if request.method == "POST":
         room.delete()
-        return redirect('home')
-    return render(request, 'base/delete.html', {'obj':room})
+        return redirect("home")
+    return render(request, "base/delete.html", {"obj": room})
