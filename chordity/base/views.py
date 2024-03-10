493e9be9c8a6a34a02a690dbd613ee331569e0b8
Rex Herndon <herndonrex1@gmail.com>
2024-03-10 13:49:39 -0500

    test commit to pull branch

--- chordity/base/views.py
+++ chordity/base/views.py
@@ -19,49 +19,53 @@ from .forms import RoomForm
 
 
 def loginPage(request):
-    page = 'login'
+    page = "login"
     if request.user.is_authenticated:
-        return redirect('home')
+        return redirect("home")
 
     if request.method == "POST":
         username = request.POST.get("username").lower()
         password = request.POST.get("password")
-        
+
         try:
             user = User.objects.get(username=username)
         except:
-            messages.error(request, 'User does not exist')
+            messages.error(request, "User does not exist")
 
         user = authenticate(request, username=username, password=password)
 
-        if user is not None: 
+        if user is not None:
             login(request, user)
-            return redirect('home')
+            return redirect("home")
         else:
-            messages.error(request, 'Username or password does not exist.')
-        
-    context = {'page': page}
+            messages.error(request, "Username or password does not exist.")
+
+    context = {"page": page}
     return render(request, "base/login_register.html", context)
+
 
 def logoutUser(request):
     logout(request)
-    return redirect('home')
+    return redirect("home")
+
 
 def registerPage(request):
     form = UserCreationForm()
-    
-    if request.method == 'POST':
+
+    if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
             user.username = user.username.lower()
             user.save()
             login(request, user)
-            return redirect('home')
+            return redirect("home")
         else:
-            messages.error(request, 'An error has occured during registration. Please try again.')
-            
-    return render(request, 'base/login_register.html', {'form': form})
+            messages.error(
+                request, "An error has occured during registration. Please try again."
+            )
+
+    return render(request, "base/login_register.html", {"form": form})
 
 
 def home(request):
@@ -91,6 +95,7 @@ def room(request, pk):
 
     return render(request, "base/room.html", context)
 
+
 @login_required(login_url="login")
 def createRoom(request):
     form = RoomForm()
@@ -111,7 +116,7 @@ def updateRoom(request, pk):
     form = RoomForm(instance=room)
 
     if request.user != room.host:
-        return HttpResponse('User does not have permissions to access this.')
+        return HttpResponse("User does not have permissions to access this.")
 
     if request.method == "POST":
         form = RoomForm(request.POST, instance=room)
@@ -128,7 +133,7 @@ def deleteRoom(request, pk):
     room = Room.objects.get(id=pk)
 
     if request.user != room.host:
-        return HttpResponse('User does not have permissions to access this.')
+        return HttpResponse("User does not have permissions to access this.")
 
     if request.method == "POST":
         room.delete()
